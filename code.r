3f5ec5d420fb7b8bb1bb017b8a3365873859c16e
Natalie <117225846+nf-97@users.noreply.github.com>
2022-12-01 00:08:45 +0000

    changed formatting of code

--- code.r
+++ code.r
@@ -1,15 +1,15 @@
 # create a dataset for fisheries
 Year <- rep(c("2015","2016", "2017", "2018", "2019", "2020"),5)
 Stage <-  c(rep("Juvenile" , 6) , rep("Immature 1" , 6) , rep("Immature 2" , 6) , rep("Immature 3" , 6), rep("Adult", 6))
-value <- c(0,1923.916288, 1429.469802, 1062.096063,	789.1373747,	586.3290694,
-           0,	0,	659.9032868,	490.3081421,	364.2989496,	270.6741195,
-           0,	0,	0,	490.3081421,	364.2989496,	270.6741195,
-           0,	0,	0,	0,	364.2989496,	270.6741195,
-           8512,	6324.416,	4699.041088,	3491.387528,	2594.100934,	2198.091113)
+value <- c(0, 1923.916288, 1429.469802, 1062.096063, 789.1373747, 586.3290694,
+           0,	0, 659.9032868,	490.3081421, 364.2989496, 270.6741195,
+           0,	0, 0,	490.3081421, 364.2989496, 270.6741195,
+           0,	0, 0,	0, 364.2989496, 270.6741195,
+           8512,	6324.416,	4699.041088,	3491.387528,	2594.100934, 2198.091113)
 total <- 2*(value)
 fishery <- data.frame(Year,Stage,value)
 
-# Stacked bar chart for fisheries dataset
+# stacked bar chart for fisheries dataset
 library(ggplot2)
 
 cbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
@@ -27,35 +27,36 @@ scale_y_continuous(breaks=seq(0, 20000, 2500),
 expand = c(0,0), 
 limits = c(0,20000)) 
 
-#dataset for no fisheries
+# dataset for no fisheries
 Year_no <- rep(c("2015","2016", "2017", "2018", "2019", "2020"),5)
 Stage_no <-  c(rep("Juvenile" , 6) , rep("Immature 1" , 6) , rep("Immature 2" , 6) , rep("Immature 3" , 6), rep("Adult", 6))
-value_no <- c(0,	2693.477696,	2001.253928,	1486.931669,	1104.79023,	820.8591407,
-           0,	0,	923.8628497,	686.4300973,	510.0175623,	378.9430488, 
-           0,	0,	0,	686.4300973,	510.0175623, 378.9430488,
-           0,	0,	0,	0,	510.0175623,	378.9430488,
-           8512,	6324.416,	4699.041088,	3491.387528,	2594.100934,	2306.360042)
+value_no <- c(0, 2693.477696, 2001.253928, 1486.931669, 1104.79023, 820.8591407,
+           0,	0, 923.8628497, 686.4300973, 510.0175623,	378.9430488, 
+           0,	0, 0,	686.4300973, 510.0175623, 378.9430488,
+           0,	0, 0,	0, 510.0175623, 378.9430488,
+           8512, 6324.416, 4699.041088, 3491.387528, 2594.100934, 2306.360042)
 total_no <- 2*(value_no)
 no_fishery <- data.frame(Year_no,Stage_no,value_no)
 
-#dataset for both
+# dataset for both
 Year <- rep(c("2015","2016", "2017", "2018", "2019", "2020"),10)
 Stage <-  c(rep("Juvenile" , 6) , rep("Immature 1" , 6) , rep("Immature 2" , 6) , rep("Immature 3" , 6), rep("Adult", 6), rep("Juvenile" , 6) , rep("Immature 1" , 6) , rep("Immature 2" , 6) , rep("Immature 3" , 6), rep("Adult", 6))
 Data <- c(rep("Fishery Present", 30), rep("Fishery Absent", 30))
-value_comb <- c(0,1923.916288, 1429.469802, 1062.096063,	789.1373747,	586.3290694,
-           0,	0,	659.9032868,	490.3081421,	364.2989496,	270.6741195,
-           0,	0,	0,	490.3081421,	364.2989496,	270.6741195,
-           0,	0,	0,	0,	364.2989496,	270.6741195,
-           8512,	6324.416,	4699.041088,	3491.387528,	2594.100934,	2198.091113, 
-           0,	2693.477696,	2001.253928,	1486.931669,	1104.79023,	820.8591407,
+value_comb <- c(0,1923.916288, 1429.469802, 1062.096063, 789.1373747, 586.3290694,
+           0,	0,	659.9032868, 490.3081421,	364.2989496, 270.6741195,
+           0,	0, 0,	490.3081421, 364.2989496,	270.6741195,
+           0,	0, 0, 0, 364.2989496,	270.6741195,
+           8512, 6324.416, 4699.041088,	3491.387528, 2594.100934,	2198.091113, 
+           0,	2693.477696, 2001.253928,	1486.931669, 1104.79023, 820.8591407,
            0,	0,	923.8628497,	686.4300973,	510.0175623,	378.9430488, 
-           0,	0,	0,	686.4300973,	510.0175623, 378.9430488,
-           0,	0,	0,	0,	510.0175623,	378.9430488,
-           8512,	6324.416,	4699.041088,	3491.387528,	2594.100934,	2306.360042)
+           0,	0, 0,	686.4300973, 510.0175623, 378.9430488,
+           0,	0, 0,	0, 510.0175623,	378.9430488,
+           8512, 6324.416, 4699.041088,	3491.387528, 2594.100934,	2306.360042)
 total <- 2*(value_comb)
 combined <- data.frame(Year,Stage,Data, value_comb)
 
-#stacked bar chart with both sets of data
+#stacked barchart with both sets of data
+library(ggplot2)
 cbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 graph_combined <- ggplot(combined, aes(fill=Stage, y=value_comb, x=Year)) + 
 geom_bar(position="stack", stat="identity") + 
@@ -70,17 +71,13 @@ legend.text = element_text(size=13, colour="black"),
 axis.text = element_text(size=13, colour="black")) +
 scale_y_continuous(breaks=seq(0, 20000, 2500),expand = c(0,0), limits = c(0,10000))                 
 
+# see graph
 graph_combined
 
-#stats - fishers exact test to compare predicted stable age distributions
+# stats - comparing predicted stable age distribution 
 Predicted <- c("62.50%", "23.66%", "11.19%" , "2.65%", "0.00%")
 Projected <- c("62.10%", "24.40%", "10.60%", "2.90%", "0.00%")
-Age <- rep(c("0","1", "2", "3", "4"),2)
-prop <- data.frame(Predicted, Projected, Age)
+prop <- data.frame(Predicted, Projected)
 
-Combo <- c(Predicted, Projected)
-labels <-  c(rep("Predicted Stable Age Distribution", 5), rep("Projected Stable Age Distribution", 5))
-combo2 <- data.frame(Combo, labels, Age)
-
-#statistical test
-fisher.test(prop$Predicted, prop$Projected)
+# statistical test
+fisher.test(prop$Predicted, prop$Projected) 
